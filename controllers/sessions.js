97f7972da9499b53648e25735b12db4c2679c675
Brooke <Brooke@Mac.local>
2018-10-16 20:43:03 -0400

    heroku

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,28 +1,28 @@
-const express = require('express')
-const sessions = express.Router()
-const User = require('../models/users.js')
-const passport = require('passport')
+// const express = require('express')
+// const sessions = express.Router()
+// const User = require('../models/users.js')
+// const passport = require('passport')
 
-sessions.get('/new', (req, res) => {
-  res.render('sessions/new.ejs')
-})
+// sessions.get('/new', (req, res) => {
+//   res.render('sessions/new.ejs')
+// })
+//
+// sessions.post('/', (req, res)=>{
+//     User.findOne({ username: req.body.username }, (err, foundUser) => {
+//         if(req.body.password == foundUser.password){
+//           req.session.currentUser = foundUser
+//             res.redirect('/pets')
+//         } else {
+//           res.send('<a href="/">wrong password</a>')
+//         }
+//     });
+// });
+//
+// sessions.delete('/', (req, res)=>{
+//     req.session.destroy(() => {
+//         res.redirect('/pets')
+//     })
+// })
+//
 
-sessions.post('/', (req, res)=>{
-    User.findOne({ username: req.body.username }, (err, foundUser) => {
-        if(req.body.password == foundUser.password){
-          req.session.currentUser = foundUser
-            res.redirect('/pets')
-        } else {
-          res.send('<a href="/">wrong password</a>')
-        }
-    });
-});
-
-sessions.delete('/', (req, res)=>{
-    req.session.destroy(() => {
-        res.redirect('/pets')
-    })
-})
-
-
-module.exports = sessions
+// module.exports = sessions
