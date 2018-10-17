67acef038d01e7ff2ad558f8fcbc080e71bfb5c9
Brooke <Brooke@Mac.local>
2018-10-17 19:23:08 -0400

    final

--- models/seed.js
+++ models/seed.js
@@ -8,7 +8,7 @@ let seedPets = [
   },
   {
     name: 'Peach',
-    about: 'Black/White Female Pit Bull Terrier Mix. Medium size. She is about 4 years old and loves to run and play. She usually comes when you call her name and is very friendly. ',
+    about: 'Black/White Female Pit Bull Terrier Mix. Medium size. She is about 4 years old and loves to run and play. She usually comes when you call her name and is very friendly. Doggo ipsum woofer you are doin me a concern pats. Dat tungg tho tungg noodle horse heckin angery woofer floofs most angery pupper I have ever seen, blep what a nice floof most angery pupper ',
     img: "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/42601452/1/?bust=1535575929resize=width:200,height:250,fit:crop",
     missing: 'October 8, 2018',
     type: 'dog',
@@ -77,32 +77,19 @@ let seedPets = [
     type: 'cat',
   },
   {
-    name: 'June',
-    about: 'Coonhound - Doggo ipsum woofer you are doin me a concern pats. Dat tungg tho tungg noodle horse heckin angery woofer floofs most angery pupper I have ever seen, blep what a nice floof most angery pupper',
-    img: 'https://cdn1.imggmi.com/uploads/2018/10/18/ce193be5bbab0b341c68f6e0f485f5b8-full.pngresize=width:200,height:250,fit:crop',
-    missing: 'October 16, 2018',
-    type: 'dog',
-  },
-  {
     name: 'Lil Guy',
     about: 'Boxer - Doggo Ipsum boof h*ck tungg heck dat tungg tho heck woofer doing me a frighten fat boi, porgo shibe long bois lotsa pats ur givin me a spook wrinkler. Borking lotsa pats shoob doggorino borkf, stop it fren. Doge fat boi shoob. ',
-    img: 'https://cdn1.imggmi.com/uploads/2018/10/18/2e1ab4abb697ec680c0a7554bfea5559-full.pngresize=width:200,height:250,fit:crop',
+    img: 'https://cdn1.imggmi.com/uploads/2018/10/18/2387709267dd69b07fa075bc1e9b3b3b-full.png',
     missing: 'October 14, 2018',
     type: 'dog',
   },
   {
     name: 'Spice',
     about: 'Domestic Shorthair - Cat ipsum dolor sit amet, himalayan kitten so cougar. Tabby donskoy. Tom donskoy for devonshire rex siamese, tomcat maine coon burmese. Tiger british shorthair turkish angora, bobcat savannah so ragdoll. Kitten. Tiger jaguar scottish fold yet tomcat. ',
-    img: 'https://cdn1.imggmi.com/uploads/2018/10/18/98908bffaa704ba87b1a3ec1b48c5575-full.pngresize=width:200,height:250,fit:crop',
+    img: 'https://cdn1.imggmi.com/uploads/2018/10/18/98908bffaa704ba87b1a3ec1b48c5575-full.png',
     missing: 'October 14, 2018',
     type: 'cat',
   },
-
-
-
-
-
-
 
 ]
 
